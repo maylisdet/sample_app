7dd9a6c84f0635016e49effa60971a964de5236b
maylisdet <maylisdetalhouet@gmail.com>
2020-06-08 11:42:18 +0200

    Creation users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/help'
+  # get 'users/new'
+  # get 'pages/home'
+  get '/home', :to => 'pages#home'
+  get '/contact', :to => 'pages#contact'
+  get '/about',   :to => 'pages#about'
+  get '/help',    :to => 'pages#help'
+  get '/signup', :to => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
